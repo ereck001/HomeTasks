b00d8855680696e04024c31e46a76a5683f19a84
ereck <ereck.c.santos@gmail.com>
2025-04-23 18:45:00 -0300

    exclusão do diretório db_queries

--- db_queries/SQLQuery_1.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS PurchasedProducts(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    isPurchased BOOLEAN NOT NULL,
-    doneAt DATE
-);
-
--- db_queries/SQLQuery_2.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS Tasks(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    description TEXT,
-    isPurchased BOOLEAN NOT NULL,
-    doneAt DATE
-);
